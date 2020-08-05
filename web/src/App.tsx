27e8287dd17656d2f65f8878a9a941ce77bc5fcb
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-08-05 00:15:27 -0300

    Responsive props & landing page finished & Router added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import './assets/styles/global.css';
+import Routes from './routes';
+
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const TeacherForm: React.FC = () => {
+  return <h1>Teacher Form</h1>;
+}
+
+export default TeacherForm;
