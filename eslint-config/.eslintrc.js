8129eadd7068e34c799cf2f1673c1e4b90e38c6e
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-08-10 15:37:35 -0300

    Added eslint configs

---
+++ eslint-config/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb",
+        "plugin:react/recommended",
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:import/typescript'
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 16,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint",
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": "error",
+        "class-methods-use-this": "off",
+        "import/prefer-default-export": "off",
+
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '_',
+          },
+        ],
+
+        "import/no-unresolved": "error",
+        "import/extensions": ["error", {
+          extensions: [".ts", ".tsx", ".js", ".jsx"]
+        }]
+    },
+    "settings": {
+      "import/resolver": {
+        "typescript": {
+          "project": "**/tsconfig.json"
+        }
+      }
+    }
+};
